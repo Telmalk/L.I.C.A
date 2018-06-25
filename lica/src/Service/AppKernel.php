7672cad0b94119bd37de73abb3da39cf907eba79
mallano <maelallano1@gmail.com>
2018-06-25 18:10:27 +0200

    AAHHHHHH

---
+++ lica/src/Service/AppKernel.php
@@ -0,0 +1,18 @@
+<?php
+///**
+// * Created by PhpStorm.
+// * User: mallano
+// * Date: 25/06/2018
+// * Time: 16:58
+// */
+//
+//class AppKernel extends Kernel
+//{
+//    public function registerBundles()
+//    {
+//        return array(
+//            // ...
+//            new Vich\UploaderBundle\VichUploaderBundle(),
+//        );
+//    }
+//}
