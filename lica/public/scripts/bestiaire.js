44be339978edb9104414896d61c8aa8fe89ef157
mallano <maelallano1@gmail.com>
2018-06-28 14:56:57 +0200

    bestiaire page done I guess

--- lica/public/scripts/bestiaire.js
+++ lica/public/scripts/bestiaire.js
@@ -1,15 +1,27 @@
 const selection = document.querySelector('.bestiaire-selection-scrolldown-title');
 const scrolldown = document.querySelector('.bestiaire-selection-scrolldown-menu');
-const sure = document.querySelectorAll('.bestiaire-aliens-card-adopter');
+const sure = document.querySelectorAll('.bestiaire-aliens-card-button');
+const adopt = document.querySelector('.bestiaire-aliens-card-adopter');
+const aliens_details = document.querySelector('.bestiaire-aliens-modal');
+const validate = document.querySelector('.bestiaire-aliens-card-adopter-overlay');
+const exit_button = document.querySelector('.bestiaire-aliens-modal-content-cross');
 
 
 
 selection.addEventListener('click', () => {
-    scrolldown.classList.toggle('is-open')
+    scrolldown.classList.toggle('is-open');
 });
 
 for (let i = 0; i < sure.length; i++) {
-    sure[i].addEventListener('click', function() {
-        this.nextElementSibling.classList.toggle('is-open');
+    sure[i].addEventListener('click', () => {
+        aliens_details.style.display = 'block';
     });
 }
+
+adopt.addEventListener('click', () => {
+    validate.classList.toggle('is-open');
+});
+
+exit_button.addEventListener('click', () => {
+    aliens_details.style.display = 'none';
+});
