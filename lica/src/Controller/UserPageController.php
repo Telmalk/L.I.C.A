b7c0341d536b1218e38cdaa335d5fba721dab663
mallano <maelallano1@gmail.com>
2018-06-29 02:26:25 +0200

    userPage updated betUser

--- lica/src/Controller/UserPageController.php
+++ lica/src/Controller/UserPageController.php
@@ -12,6 +12,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\User;
 use App\Entity\Alien;
+use App\Entity\Bet;
+use App\Entity\Fight;
 
 class UserPageController extends Controller
 {
@@ -28,12 +30,38 @@ class UserPageController extends Controller
         $entityManagerAlien = $this->getDoctrine()->getManager();
         $alien = $entityManagerAlien->getRepository(Alien::class)->findAll();
 
-        //var_dump($alien[0]->getUser()->getId());exit;
+        $entityManagerBet = $this->getDoctrine()->getManager();
+        $bet = $entityManagerBet->getRepository(Bet::class)->findAll();
+
+        $entityManagerFight = $this->getDoctrine()->getManager();
+        $fight = $entityManagerFight->getRepository(Fight::class)->findAll();
+
         $alienOfUser = [];
         for ($i = 0; $i < count($alien); $i++) {
             if ($alien[$i]->getUser()) {
                 if ($alien[$i]->getUser()->getId() === $user->getId()) {
                     $alienOfUser[] = $alien[$i];
+                }
+            }
+        }
+
+        $betOfUser = [];
+        for ($i = 0; $i < count($bet); $i++) {
+            if ($bet[$i]->getIdUser()) {
+                if ($bet[$i]->getIdUser()->getId() === $user->getId()) {
+                    $idFight = $bet[$i]->getIdFight()->getId();
+                    $fight = $entityManagerFight->getRepository(Fight::class)->find($idFight);
+                    $betOfUser[] = [
+                      "userPseudo1" => $fight->getUser1()->getPseudo(),
+                      "alienName1" => $fight->getAlien1()->getName(),
+                      "oddFighter1" => $fight->getOddFighter1(),
+                      "userPseudo2" => $fight->getUser2()->getPseudo(),
+                      "alienName2" => $fight->getAlien2()->getName(),
+                      "oddFighter2" => $fight->getOddFighter2(),
+                      "date" => $fight->getDate()->format('d/m/Y'),
+                      "betTarget" => $bet[$i]->getBetTarget(),
+                      "wager" => $bet[$i]->getWager()
+                    ];
                 }
             }
         }
@@ -49,7 +77,8 @@ class UserPageController extends Controller
             'userWin' => $user->getWin(),
             'userDefeat' => $user->getDefeat(),
             'userDescription' => $user->getDescription(),
-            'alienOfUser' => $alienOfUser
+            'alienOfUser' => $alienOfUser,
+            'betOfUser' => $betOfUser
         ]);
     }
 }
